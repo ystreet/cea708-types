1549c73ff74794e89db99c5b3f95ffa138545437
Matthew Waters <matthew@centricular.com>
2025-04-07 10:31:47 +1000

    tests: separate packet tests into 3 categories, both, parse, write
    - 'Both' can be parsed and written and produce the same result.
    - 'Parse' can be parsed but may produce a slightly different result when
      written.
    - 'Write' can be written but may produce a slightly different result
      when parsed.

--- src/lib.rs
+++ src/lib.rs
@@ -1041,7 +1041,7 @@ mod test {
         tables::Code::LatinLowerH,
     ];
 
-    static TEST_CC_DATA: [TestCCData; 9] = [
+    static TEST_CC_DATA: [TestCCData; 11] = [
         // simple packet with a single service and single code
         TestCCData {
             framerate: Framerate::new(25, 1),
@@ -1093,32 +1093,6 @@ mod test {
             ],
             cea608: &[],
         },
-        // two packets with a single service and one code split across both packets
-        TestCCData {
-            framerate: Framerate::new(25, 1),
-            cc_data: &[
-                &[0x80 | 0x40 | 0x01, 0xFF, 0xFF, 0x02, 0x21],
-                &[0x80 | 0x40 | 0x01, 0xFF, 0xFE, 0x41, 0x00],
-            ],
-            packets: &[PacketData {
-                sequence_no: 0,
-                services: &[ServiceData {
-                    service_no: 1,
-                    codes: &[tables::Code::LatinCapitalA],
-                }],
-            }],
-            cea608: &[],
-        },
-        // simple packet with a single null service
-        TestCCData {
-            framerate: Framerate::new(25, 1),
-            cc_data: &[&[0x80 | 0x40 | 0x01, 0xFF, 0xFF, 0x01, 0x00]],
-            packets: &[PacketData {
-                sequence_no: 0,
-                services: &[],
-            }],
-            cea608: &[],
-        },
         // DTVCCPacket with two services
         TestCCData {
             framerate: Framerate::new(25, 1),
@@ -1150,162 +1124,6 @@ mod test {
             }],
             cea608: &[],
         },
-        // cc_data with two DTVCCPacket
-        TestCCData {
-            framerate: Framerate::new(25, 1),
-            cc_data: &[&[
-                0x80 | 0x40 | 0x04,
-                0xFF,
-                0xFF,
-                0x02,
-                0x21,
-                0xFE,
-                0x41,
-                0x00,
-                0xFF,
-                0x42,
-                0x41,
-                0xFE,
-                0x42,
-                0x00,
-            ]],
-            packets: &[
-                PacketData {
-                    sequence_no: 0,
-                    services: &[ServiceData {
-                        service_no: 1,
-                        codes: &[tables::Code::LatinCapitalA],
-                    }],
-                },
-                PacketData {
-                    sequence_no: 1,
-                    services: &[ServiceData {
-                        service_no: 2,
-                        codes: &[tables::Code::LatinCapitalB],
-                    }],
-                },
-            ],
-            cea608: &[],
-        },
-        // two packets with a single service and one code split across both packets with 608
-        // padding data
-        TestCCData {
-            framerate: Framerate::new(25, 1),
-            cc_data: &[
-                &[
-                    0x80 | 0x40 | 0x03,
-                    0xFF,
-                    0xFC,
-                    0x61,
-                    0x62,
-                    0xFD,
-                    0x63,
-                    0x64,
-                    0xFF,
-                    0x02,
-                    0x21,
-                ],
-                &[
-                    0x80 | 0x40 | 0x03,
-                    0xFF,
-                    0xFC,
-                    0x41,
-                    0x42,
-                    0xFD,
-                    0x43,
-                    0x44,
-                    0xFE,
-                    0x41,
-                    0x00,
-                ],
-            ],
-            packets: &[PacketData {
-                sequence_no: 0,
-                services: &[ServiceData {
-                    service_no: 1,
-                    codes: &[tables::Code::LatinCapitalA],
-                }],
-            }],
-            cea608: &[
-                &[Cea608::Field1(0x61, 0x62), Cea608::Field2(0x63, 0x64)],
-                &[Cea608::Field1(0x41, 0x42), Cea608::Field2(0x43, 0x44)],
-            ],
-        },
-        // cc_data with two DTVCCPacket. First packet spans two cc_data frames, second is whole
-        // contained within the second cc_data frame
-        TestCCData {
-            framerate: Framerate::new(25, 1),
-            cc_data: &[
-                &[0x80 | 0x40 | 0x01, 0xFF, 0xFF, 0x02, 0x21],
-                &[
-                    0x80 | 0x40 | 0x03,
-                    0xFF,
-                    0xFE,
-                    0x41,
-                    0x00,
-                    0xFF,
-                    0x42,
-                    0x41,
-                    0xFE,
-                    0x42,
-                    0x00,
-                ],
-            ],
-            packets: &[
-                PacketData {
-                    sequence_no: 0,
-                    services: &[ServiceData {
-                        service_no: 1,
-                        codes: &[tables::Code::LatinCapitalA],
-                    }],
-                },
-                PacketData {
-                    sequence_no: 1,
-                    services: &[ServiceData {
-                        service_no: 2,
-                        codes: &[tables::Code::LatinCapitalB],
-                    }],
-                },
-            ],
-            cea608: &[],
-        },
-    ];
-
-    #[test]
-    fn cc_data_parse() {
-        test_init_log();
-        for (i, test_data) in TEST_CC_DATA.iter().enumerate() {
-            log::info!("parsing {i}: {test_data:?}");
-            let mut parser = CCDataParser::new();
-            if !test_data.cea608.is_empty() {
-                parser.handle_cea608();
-            }
-            let mut expected_iter = test_data.packets.iter();
-            let mut cea608_iter = test_data.cea608.iter();
-            for data in test_data.cc_data.iter() {
-                debug!("pushing {data:?}");
-                parser.push(data).unwrap();
-                while let Some(packet) = parser.pop_packet() {
-                    let expected = expected_iter.next().unwrap();
-                    assert_eq!(expected.sequence_no, packet.sequence_no());
-                    let services = packet.services();
-                    let mut expected_service_iter = expected.services.iter();
-                    for parsed_service in services.iter() {
-                        let expected_service = expected_service_iter.next().unwrap();
-                        assert_eq!(parsed_service.number(), expected_service.service_no);
-                        assert_eq!(expected_service.codes, parsed_service.codes());
-                    }
-                    assert!(expected_service_iter.next().is_none());
-                }
-                assert_eq!(parser.cea608().as_ref(), cea608_iter.next());
-            }
-            assert!(parser.pop_packet().is_none());
-            assert!(expected_iter.next().is_none());
-            assert!(cea608_iter.next().is_none());
-        }
-    }
-
-    static WRITE_CC_DATA: [TestCCData; 8] = [
         // simple packet with a single service and single code
         TestCCData {
             framerate: Framerate::new(25, 1),
@@ -1332,7 +1150,7 @@ mod test {
             }],
             cea608: &[],
         },
-        // packet with a full service service
+        // packet with a full service
         TestCCData {
             framerate: Framerate::new(25, 1),
             cc_data: &[&[
@@ -1406,14 +1224,7 @@ mod test {
             packets: &[],
             cea608: &[&[Cea608::Field1(0x41, 0x42)]],
         },
-        // simple packet with only cea608 field 1 data
-        TestCCData {
-            framerate: Framerate::new(25, 1),
-            cc_data: &[&[0x80 | 0x40 | 0x02, 0xFF, 0xFC, 0x80, 0x80, 0xFD, 0x41, 0x42]],
-            packets: &[],
-            cea608: &[&[Cea608::Field2(0x41, 0x42)]],
-        },
-        // simple packet that will span two outputs
+        // full packet spanning multiple outputs
         TestCCData {
             framerate: Framerate::new(60, 1),
             cc_data: &[
@@ -1490,7 +1301,7 @@ mod test {
                     codes: TEST_CODES.first_chunk::<31>().unwrap(),
                 }],
             }],
-            cea608: &[&[Cea608::Field1(0x20, 0x42), Cea608::Field2(0x21, 0x43)]],
+            cea608: &[&[Cea608::Field1(0x20, 0x42)], &[Cea608::Field2(0x21, 0x43)]],
         },
         // simple packet with multiple cea608 that will span two outputs
         TestCCData {
@@ -1499,10 +1310,7 @@ mod test {
                 &[0x80 | 0x40 | 0x02, 0xFF, 0xFC, 0x20, 0x42, 0xFD, 0x21, 0x43],
                 &[0x80 | 0x40 | 0x02, 0xFF, 0xFC, 0x22, 0x44, 0xFD, 0x23, 0x45],
             ],
-            packets: &[PacketData {
-                sequence_no: 3,
-                services: &[],
-            }],
+            packets: &[],
             cea608: &[
                 &[Cea608::Field1(0x20, 0x42), Cea608::Field2(0x21, 0x43)],
                 &[Cea608::Field1(0x22, 0x44), Cea608::Field2(0x23, 0x45)],
@@ -1785,24 +1593,216 @@ mod test {
                     },
                 ],
             }],
-            cea608: &[&[
-                Cea608::Field1(0x20, 0x42),
-                Cea608::Field2(0x21, 0x43),
-                Cea608::Field1(0x22, 0x44),
-                Cea608::Field2(0x23, 0x45),
-                Cea608::Field1(0x24, 0x46),
-                Cea608::Field2(0x25, 0x47),
-                Cea608::Field1(0x26, 0x48),
-                Cea608::Field2(0x27, 0x49),
+            cea608: &[
+                &[Cea608::Field1(0x20, 0x42)],
+                &[Cea608::Field2(0x21, 0x43)],
+                &[Cea608::Field1(0x22, 0x44)],
+                &[Cea608::Field2(0x23, 0x45)],
+                &[Cea608::Field1(0x24, 0x46)],
+                &[Cea608::Field2(0x25, 0x47)],
+                &[Cea608::Field1(0x26, 0x48)],
+                &[Cea608::Field2(0x27, 0x49)],
+            ],
+        },
+    ];
+
+    static PARSE_CC_DATA: [TestCCData; 5] = [
+        // two packets with a single service and one code split across both packets
+        TestCCData {
+            framerate: Framerate::new(25, 1),
+            cc_data: &[
+                &[0x80 | 0x40 | 0x01, 0xFF, 0xFF, 0x02, 0x21],
+                &[0x80 | 0x40 | 0x01, 0xFF, 0xFE, 0x41, 0x00],
+            ],
+            packets: &[PacketData {
+                sequence_no: 0,
+                services: &[ServiceData {
+                    service_no: 1,
+                    codes: &[tables::Code::LatinCapitalA],
+                }],
+            }],
+            cea608: &[],
+        },
+        // simple packet with a single null service
+        TestCCData {
+            framerate: Framerate::new(25, 1),
+            cc_data: &[&[0x80 | 0x40 | 0x01, 0xFF, 0xFF, 0x01, 0x00]],
+            packets: &[PacketData {
+                sequence_no: 0,
+                services: &[],
+            }],
+            cea608: &[],
+        },
+        // cc_data with two DTVCCPacket
+        TestCCData {
+            framerate: Framerate::new(25, 1),
+            cc_data: &[&[
+                0x80 | 0x40 | 0x04,
+                0xFF,
+                0xFF,
+                0x02,
+                0x21,
+                0xFE,
+                0x41,
+                0x00,
+                0xFF,
+                0x42,
+                0x41,
+                0xFE,
+                0x42,
+                0x00,
             ]],
+            packets: &[
+                PacketData {
+                    sequence_no: 0,
+                    services: &[ServiceData {
+                        service_no: 1,
+                        codes: &[tables::Code::LatinCapitalA],
+                    }],
+                },
+                PacketData {
+                    sequence_no: 1,
+                    services: &[ServiceData {
+                        service_no: 2,
+                        codes: &[tables::Code::LatinCapitalB],
+                    }],
+                },
+            ],
+            cea608: &[],
+        },
+        // two packets with a single service and one code split across both packets with 608
+        // padding data
+        TestCCData {
+            framerate: Framerate::new(25, 1),
+            cc_data: &[
+                &[
+                    0x80 | 0x40 | 0x03,
+                    0xFF,
+                    0xFC,
+                    0x61,
+                    0x62,
+                    0xFD,
+                    0x63,
+                    0x64,
+                    0xFF,
+                    0x02,
+                    0x21,
+                ],
+                &[
+                    0x80 | 0x40 | 0x03,
+                    0xFF,
+                    0xFC,
+                    0x41,
+                    0x42,
+                    0xFD,
+                    0x43,
+                    0x44,
+                    0xFE,
+                    0x41,
+                    0x00,
+                ],
+            ],
+            packets: &[PacketData {
+                sequence_no: 0,
+                services: &[ServiceData {
+                    service_no: 1,
+                    codes: &[tables::Code::LatinCapitalA],
+                }],
+            }],
+            cea608: &[
+                &[Cea608::Field1(0x61, 0x62), Cea608::Field2(0x63, 0x64)],
+                &[Cea608::Field1(0x41, 0x42), Cea608::Field2(0x43, 0x44)],
+            ],
+        },
+        // cc_data with two DTVCCPacket. First packet spans two cc_data frames, second is whole
+        // contained within the second cc_data frame
+        TestCCData {
+            framerate: Framerate::new(25, 1),
+            cc_data: &[
+                &[0x80 | 0x40 | 0x01, 0xFF, 0xFF, 0x02, 0x21],
+                &[
+                    0x80 | 0x40 | 0x03,
+                    0xFF,
+                    0xFE,
+                    0x41,
+                    0x00,
+                    0xFF,
+                    0x42,
+                    0x41,
+                    0xFE,
+                    0x42,
+                    0x00,
+                ],
+            ],
+            packets: &[
+                PacketData {
+                    sequence_no: 0,
+                    services: &[ServiceData {
+                        service_no: 1,
+                        codes: &[tables::Code::LatinCapitalA],
+                    }],
+                },
+                PacketData {
+                    sequence_no: 1,
+                    services: &[ServiceData {
+                        service_no: 2,
+                        codes: &[tables::Code::LatinCapitalB],
+                    }],
+                },
+            ],
+            cea608: &[],
+        },
+    ];
+
+    #[test]
+    fn cc_data_parse() {
+        test_init_log();
+        for (i, test_data) in TEST_CC_DATA.iter().chain(PARSE_CC_DATA.iter()).enumerate() {
+            log::info!("parsing {i}: {test_data:?}");
+            let mut parser = CCDataParser::new();
+            if !test_data.cea608.is_empty() {
+                parser.handle_cea608();
+            }
+            let mut expected_iter = test_data.packets.iter();
+            let mut cea608_iter = test_data.cea608.iter();
+            for data in test_data.cc_data.iter() {
+                debug!("pushing {data:?}");
+                parser.push(data).unwrap();
+                while let Some(packet) = parser.pop_packet() {
+                    let expected = expected_iter.next().unwrap();
+                    assert_eq!(expected.sequence_no, packet.sequence_no());
+                    let services = packet.services();
+                    let mut expected_service_iter = expected.services.iter();
+                    for parsed_service in services.iter() {
+                        let expected_service = expected_service_iter.next().unwrap();
+                        assert_eq!(parsed_service.number(), expected_service.service_no);
+                        assert_eq!(expected_service.codes, parsed_service.codes());
+                    }
+                    assert!(expected_service_iter.next().is_none());
+                }
+                assert_eq!(parser.cea608().as_ref(), cea608_iter.next());
+            }
+            assert!(parser.pop_packet().is_none());
+            assert!(expected_iter.next().is_none());
+            assert!(cea608_iter.next().is_none());
+        }
+    }
+
+    static WRITE_CC_DATA: [TestCCData; 1] = [
+        // simple packet with only cea608 field 2 data
+        TestCCData {
+            framerate: Framerate::new(25, 1),
+            cc_data: &[&[0x80 | 0x40 | 0x02, 0xFF, 0xFC, 0x80, 0x80, 0xFD, 0x41, 0x42]],
+            packets: &[],
+            cea608: &[&[Cea608::Field2(0x41, 0x42)]],
         },
     ];
 
     #[test]
     fn packet_write_cc_data() {
         test_init_log();
-        for test_data in WRITE_CC_DATA.iter() {
-            log::info!("writing {test_data:?}");
+        for (i, test_data) in TEST_CC_DATA.iter().chain(WRITE_CC_DATA.iter()).enumerate() {
+            log::info!("writing {i}: {test_data:?}");
             let mut packet_iter = test_data.packets.iter();
             let mut cea608_iter = test_data.cea608.iter();
             let mut writer = CCDataWriter::default();
